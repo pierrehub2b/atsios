dd0c444c590cec706b0bb77c61ceaad7fec1ff10
pierre huber <pierrehuber@yahoo.fr>
2019-08-20 16:57:30 +0200

    add driver host info

--- atsiosUITests/atsiosUITests.swift
+++ atsiosUITests/atsiosUITests.swift
@@ -583,7 +583,9 @@ class atsiosUITests: XCTestCase {
         
         // Start HTTP server to listen on the port
         try! server.start()
-        fputs("atsios started on port " + String(self.port) + "\n", stderr)
+        let address = getWiFiAddress()! + ":" + String(self.port)
+        
+        fputs("ATSIOS_DRIVER_HOST:" + address + "\n", stderr)
         // Run event loop
         loop.runForever()
     }
@@ -809,5 +811,45 @@ class atsiosUITests: XCTestCase {
     func descriptionOfLastError() -> String {
         return String.init(cString: (UnsafePointer(strerror(errno))))
     }
-
+    
+    func getWiFiAddress() -> String? {
+        var address : String?
+        
+        // Get list of all interfaces on the local machine:
+        var ifaddr : UnsafeMutablePointer<ifaddrs>?
+        guard getifaddrs(&ifaddr) == 0 else { return nil }
+        guard let firstAddr = ifaddr else { return nil }
+        
+        // For each interface ...
+        for ifptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+            let interface = ifptr.pointee
+            
+            // Check for IPv4 only
+            let addrFamily = interface.ifa_addr.pointee.sa_family
+            if addrFamily == UInt8(AF_INET) {
+                
+                // Check interface name:
+                let name = String(cString: interface.ifa_name)
+                if  name == "en0" {
+                    
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                } else if name == "en1" {
+                    // Convert interface address to a human readable string:
+                    var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                    getnameinfo(interface.ifa_addr, socklen_t(interface.ifa_addr.pointee.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(1), NI_NUMERICHOST)
+                    address = String(cString: hostname)
+                }
+            }
+        }
+        freeifaddrs(ifaddr)
+        
+        return address
+    }
 }
